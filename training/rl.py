5b380a084f99545dc0a6458af1abd5af3239b878
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2019-07-29 17:39:03 +0900

    ♻️ Put spece after ' , ' in rl.py

--- training/rl.py
+++ training/rl.py
@@ -12,22 +12,23 @@ from parameters import param as par
 
 #STDP reinforcement learning curve
 def rl(t):
-	
-	if t>0:
-		return -par.kNegativeReinforcement_*np.exp(-float(t)/par.kNegativeTau_)
-	if t<=0:
-		return par.kPositiveReinforcement_*np.exp(float(t)/par.kPositiveTau_)
+	#back time
+	if t > 0:
+		return -par.kNegativeReinforcement_ * np.exp(-float(t) / par.kNegativeTau_)
+	#fore time
+	if t <= 0:
+		return par.kPositiveReinforcement_ * np.exp(float(t) / par.kPositiveTau_)
 
 
 #STDP weight update rule
 def update(w, del_w):
-	if del_w<0:
-		return w + par.kSigma_*del_w*(w-abs(par.kMinWait_))*par.kScale_
-	elif del_w>0:
-		return w + par.kSigma_*del_w*(par.kMaxWait_-w)*par.kScale_
+	if del_w < 0:
+		return w + par.kSigma_ * del_w * (w - abs(par.kMinWait_)) * par.kScale_
+	elif del_w > 0:
+		return w + par.kSigma_ * del_w * (par.kMaxWait_ - w) * par.kScale_
 
 if __name__ == '__main__':
 	
-	print(rl(-20)*par.kSigma_)
+	print(rl(-20) * par.kSigma_)
 
 	
