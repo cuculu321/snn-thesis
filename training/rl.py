8d1eda4fe1990c9efc6cffeb36b4cdcb1e9046cd
cuculu <matiyu321@gmail.com>
2019-09-06 10:55:25 +0900

    📝 "def rl & update" in "rl"

--- training/rl.py
+++ training/rl.py
@@ -12,6 +12,19 @@ from parameters import param as par
 
 #STDP reinforcement learning curve
 def rl(t):
+	"""
+	スパイク間隔の時間
+
+	Parameters
+	----------
+	t : int
+		発火していた時刻
+
+	Returns
+	------
+	 : float
+		一般的なSTDP機能で得られる値
+	"""
 	#back time
 	if t > 0:
 		return -par.kNegativeReinforcement_ * np.exp(-float(t) / par.kNegativeTau_)
@@ -22,6 +35,20 @@ def rl(t):
 
 #STDP weight update rule
 def update(w, del_w):
+	"""
+	STDP則に基づく重みの更新
+
+	Parameters
+	----------
+	w : float
+		更新が行われるシナプスの値
+	del_w : float
+		SDTPによって導き出された値
+
+	Returns
+	------
+	 : 新しく更新される値
+	"""
 	if del_w < 0:
 		return w + par.kSigma_ * del_w * (w - abs(par.kMinWait_)) * par.kScale_
 	elif del_w > 0:
