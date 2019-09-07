78312ab3e03d5443e00821d574a256ce23cd42a1
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2019-09-07 17:59:20 +0900

    🔥 delete time data

--- 2layer_voice_reco/wav_split.py
+++ 2layer_voice_reco/wav_split.py
@@ -5,8 +5,6 @@ import soundfile as sf
 def wav_split(file_name):
     signal, samplerate = sf.read(file_name)
 
-    t = np.arange(0, len(signal) / samplerate, 1/samplerate)
-
     # 音声波形を0.04毎、オーバーラップ50%で切り出す
     time_interval = 0.02 #オーバーラップ50%なので0.02秒ごと
     cuttime = 0.04 #ポイント前後0.02秒合計0.04秒
@@ -17,14 +15,13 @@ def wav_split(file_name):
     time_data_array = []
     for cut_center in cutpoint:
         sig_data_array.append(signal[int(cut_center - cuttime / 2 * samplerate) : int(cut_center + cuttime / 2 * samplerate)])
-        time_data_array.append(t[int(cut_center - cuttime/2*samplerate) : int(cut_center + cuttime/2*samplerate)])
 
-    return sig_data_array, time_data_array
+    return sig_data_array
 
 if __name__ == '__main__':
-    splited_sig_array, splited_time_array = wav_split("./PASL-DSR/WAVES/F1/AES/F1AES2.wav")
+    splited_sig_array = wav_split("./PASL-DSR/WAVES/F1/AES/F1AES2.wav")
 
-    time = splited_time_array[int(len(splited_time_array)/2)]
+    time = np.arange(0, 40, 0.0625)
     x = splited_sig_array[int(len(splited_sig_array)/2)]
     plt.plot(time * 1000, x)
     plt.xlabel("time [ms]")
