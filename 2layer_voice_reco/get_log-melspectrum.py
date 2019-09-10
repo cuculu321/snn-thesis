cae16378cd74b828ee989e48323f29725b85fde1
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2019-09-10 11:01:33 +0900

    ♻️ change "get_mel" to "filter_humming"

--- 2layer_voice_reco/get_log-melspectrum.py
+++ 2layer_voice_reco/get_log-melspectrum.py
@@ -1,7 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_mel(signal, samplerate):
+def filter_humming(signal, samplerate):
+    """
+    音声データにハミング窓をかけて振幅スペクトルを得る
+
+    Parameters
+	----------
+	signal : list[float]
+        音声データのアナログ情報
+    samplerate : int
+        音声データのサンプルレート
+
+	Returns
+	-------
+
+    """
     # ハミング窓をかける
     hamming = np.hamming(len(signal))
     ham_signal = signal * hamming
@@ -17,7 +31,7 @@ if __name__ == '__main__':
     from wav_split import wav_split
     splited_sig_array, samplerate = wav_split("./PASL-DSR/WAVES/F1/AES/F1AES2.wav")
     signal = splited_sig_array[int(len(splited_sig_array)/2)]
-    fscale, spec = get_mel(signal, samplerate)
+    fscale, spec = filter_humming(signal, samplerate)
 
     plt.plot(fscale, spec)
     plt.xlabel("frequency [Hz]")
