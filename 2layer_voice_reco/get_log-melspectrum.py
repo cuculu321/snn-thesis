065d65c2278e13b7e04c66b9836a1b2398274fe1
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2019-09-10 10:53:17 +0900

    ✨ Filter humming window

--- 2layer_voice_reco/get_log-melspectrum.py
+++ 2layer_voice_reco/get_log-melspectrum.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_mel(signal):
+def get_mel(signal, samplerate):
     # ハミング窓をかける
     hamming = np.hamming(len(signal))
     ham_signal = signal * hamming
@@ -9,16 +9,16 @@ def get_mel(signal):
     # 振幅スペクトルを求める
     N = 2048 # FFTのサンプル数
     spec = np.abs(np.fft.fft(ham_signal, N))[:N//2]
-    fscale = np.fft.fftfreq(N, d = 1.0 / fs)[:N//2]
+    fscale = np.fft.fftfreq(N, d = 1.0 / samplerate)[:N//2]
 
     plt.plot(fscale, spec)
-    plt.ham_signallabel("frequency [Hz]")
+    plt.xlabel("frequency [Hz]")
     plt.ylabel("amplitude spectrum")
     plt.show()
 
 
 if __name__ == '__main__':
     from wav_split import wav_split
-    splited_sig_array = wav_split("./PASL-DSR/WAVES/F1/AES/F1AES2.wav")
+    splited_sig_array, samplerate = wav_split("./PASL-DSR/WAVES/F1/AES/F1AES2.wav")
     signal = splited_sig_array[int(len(splited_sig_array)/2)]
-    get_mel(signal)
+    get_mel(signal, samplerate)
