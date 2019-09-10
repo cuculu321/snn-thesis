1a510cb0238a7b6b38c65cc90bf7dbe853a26f9c
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2019-09-10 10:59:09 +0900

    ♻️ add "return" in "get_mel"

--- 2layer_voice_reco/get_log-melspectrum.py
+++ 2layer_voice_reco/get_log-melspectrum.py
@@ -11,14 +11,15 @@ def get_mel(signal, samplerate):
     spec = np.abs(np.fft.fft(ham_signal, N))[:N//2]
     fscale = np.fft.fftfreq(N, d = 1.0 / samplerate)[:N//2]
 
-    plt.plot(fscale, spec)
-    plt.xlabel("frequency [Hz]")
-    plt.ylabel("amplitude spectrum")
-    plt.show()
-
+    return fscale, spec
 
 if __name__ == '__main__':
     from wav_split import wav_split
     splited_sig_array, samplerate = wav_split("./PASL-DSR/WAVES/F1/AES/F1AES2.wav")
     signal = splited_sig_array[int(len(splited_sig_array)/2)]
-    get_mel(signal, samplerate)
+    fscale, spec = get_mel(signal, samplerate)
+
+    plt.plot(fscale, spec)
+    plt.xlabel("frequency [Hz]")
+    plt.ylabel("amplitude spectrum")
+    plt.show()
