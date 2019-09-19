b5ce4ec679a3acb311023d60c70ec7b8d1868e0b
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2019-09-19 11:44:48 +0900

    ✨ Spike generation by Db

--- 2layer_voice_reco/spike_train.py
+++ 2layer_voice_reco/spike_train.py
@@ -10,8 +10,6 @@ from numpy import interp
 from neuron import neuron
 import random
 from matplotlib import pyplot as plt
-from recep_field import rf
-import cv2
 from rl import rl
 from rl import update
 import math
@@ -19,66 +17,70 @@ from parameters import param as par
 
 def encode(potential):
 	"""
-	畳み込みデータからスパイク列を生成する
+	メル周波数スペクトラムからスパイク列を生成する
 
 	Parameters
 	----------
-	potential : float list[28, 28]
-		画像を畳み込んで得たデータ
+	potential : float list[22]
+		メル周波数スペクトラムのdB要素
 
 	Returns
 	------
-	train : boolean list[784, 200]
+	train : boolean list[22, 200]
 		畳み込みデータの値によって、スパイク率を変え出力された配列データ。
 	"""
 
 	#initializing spike train
 	train = []
 
-	for l in range(par.kPixelX_):
-		for m in range(par.kPixelX_):
-		
-			time_list = np.zeros([(par.kTime_+1), ])
+	for hz_point in range(par.kMelChannelCount_):
+	
+		time_list = np.zeros([(par.kTime_+1), ])
 
-			#calculating firing rate proportional to the membrane potential
-			freq = interp(potential[l][m], [-1.069, 2.781], [1, 20])
+		#calculating firing rate proportional to the membrane potential
+		freq = interp(potential[hz_point], [-1.069, 2.781], [1, 20])
+		print(freq)
 
-			# print freq
-			if freq <= 0:
-				print(error)
-				
-			freq1 = math.ceil(600 / freq)
-			#print("freq  1 : " + str(l) + " m : " + str(m) + "   " + str(freq))
+		# print freq
+		if freq <= 0:
+			print(error)
+			
+		freq1 = math.ceil(600 / freq)
+		#print("freq  1 : " + str(l) + " m : " + str(m) + "   " + str(freq))
 
-			#generating spikes according to the firing rate
-			spike_freq = freq1
-			if(potential[l][m] > 0):
-				while spike_freq < (par.kTime_ + 1):
-					time_list[spike_freq] = 1
-					spike_freq = spike_freq + freq1
-			train.append(time_list)
-			#print(sum(time_list))
-			#print("time_list  1 : " + str(l) + " m : " + str(m) + "   " + str(sum(time_list)))
+		#generating spikes according to the firing rate
+		spike_freq = freq1
+		if(potential[hz_point] > 0):
+			while spike_freq < (par.kTime_ + 1):
+				time_list[spike_freq] = 1
+				spike_freq = spike_freq + freq1
+		train.append(time_list)
+		#print(sum(time_list))
+		#print("time_list  1 : " + str(l) + " m : " + str(m) + "   " + str(sum(time_list)))
 	return train
 
 if __name__  == '__main__':
 	# m = []
 	# n = []
-	img = cv2.imread("mnist1/" + str(15) + ".png", 0)
+	from wav_split import wav_split
+	from get_logmelspectrum import get_log_melspectrum
 
-	potential = rf(img)
+	splited_sig_array, samplerate = wav_split("PASL-DSR/WAVES/F1/AES/F1AES2.wav")
+	signal = splited_sig_array[int(len(splited_sig_array)/2)]
+
+	f_centers, mel_spectrum = get_log_melspectrum(signal, samplerate)
 
 	# for i in potential:
 	# 	m.append(max(i))
 	# 	n.append(min(i))
 
 	# print max(m), min(n)
-	train = encode(potential)
-	f = open('look_ups/train6.txt', 'w')
+	train = encode(mel_spectrum)
+	f = open('look_ups/trainF1AES2.txt', 'w')
 	print(np.shape(train))
 
 	for i in range(201):
-		for j in range(784):
+		for j in range(22):
 			f.write(str(int(train[j][i])))
 		f.write('\n')
 
