6648c512d82b174bcb5dfe2d18b4f031c7d6990b
mitsui <b1614196@planet.kanazawa-it.ac.jp>
2020-01-10 12:10:46 +0900

    ✨ print the output synapse file name

--- 2layer_voice_reco/run-CPU/learning.py
+++ 2layer_voice_reco/run-CPU/learning.py
@@ -40,8 +40,6 @@ def learning(learning_path, synapse):
 		a = neuron()
 		layer2.append(a)
 
-	export_txt(synapse, "synapse_record/" + "initial" + create_timestamp())
-
 	for epoch in range(1):
 		for wave_file in learning_path:
 		#for wave_file in ["sounddata\F1\F1SYB01_が.wav"]:
@@ -199,7 +197,7 @@ if __name__ == "__main__":
 
 	#学習
 	#get wavefile path for learning
-	learning_path = get_learning_small_file_path()
+	learning_path = get_learningfile_path()
 	#learning_path = get_mappingfile_path()
 	#learning_path = [onedivision for a in learning_path for onedivision in a] #2次元のパスを1次元に変更
 
@@ -215,12 +213,15 @@ if __name__ == "__main__":
 		input_synaps = args[1]
 		synapse = import_synapse("synapse_record/" + str(input_synaps) + ".txt")
 
-	print(synapse)
+	initial_synapse_path = "synapse_record/" + "initial" + create_timestamp()
+	print(initial_synapse_path)
+	export_txt(synapse, initial_synapse_path)
+
 	potential_lists, synapse, layer2 = learning(learning_path, synapse)
 
-	resemble_print("synapse : ")
-	resemble_print(synapse)
-	export_txt(synapse, "synapse_record/" + create_timestamp())
+	learned_synapse_path = "synapse_record/" + create_timestamp()
+	print("export : " + learned_synapse_path)
+	export_txt(synapse, learned_synapse_path)
 
 	#対応付け
 	secondhand_wav_file = []
