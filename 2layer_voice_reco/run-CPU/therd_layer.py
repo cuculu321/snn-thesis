76bd1f49947e9e265bec100042c12d66e22f5c8d
Yuya <b1614196@planet.kanazawa-it.ac.jp>
2020-01-13 23:24:20 +0900

    ✨ use training or check modes properly

--- 2layer_voice_reco/run-CPU/therd_layer.py
+++ 2layer_voice_reco/run-CPU/therd_layer.py
@@ -10,42 +10,53 @@ if __name__ == "__main__":
 	from get_current_directory import *
 
 	args = sys.argv
-	input_synaps = args[1]
-	synapse = import_synapse("synapse_record/" + str(input_synaps) + ".txt")
+	if len(args) == 2:
+		print("run")
+	else:
+		print("-t [1-2synapse path] training 2-3 synapse")
+		print("-c [2-3synapse path] check accuracy")
 
-	secondhand_wav_file = []
-	speaker_list = [i for i in range(0, 12)]
+	mode = args[1]
+	input_synaps = args[2]
+	if mode == "-t":
+		synapse = import_synapse("synapse_record/" + str(input_synaps) + ".txt")
 
-	mapping_list = [[] for _ in range(par.kSecondLayerNuerons_)]
+		secondhand_wav_file = []
+		speaker_list = [i for i in range(0, 12)]
 
-	mapping_path = get_mappingfile_path()
-	for i in range(len(mapping_path)):
-		mapping_path[i].sort()
+		mapping_list = [[] for _ in range(par.kSecondLayerNuerons_)]
 
-	therd_neuron = []
-	mapping_list = []
+		mapping_path = get_mappingfile_path()
+		for i in range(len(mapping_path)):
+			mapping_path[i].sort()
 
-	for syllable_num in range(len(mapping_path[0])): #単音節の数(F1のファイル数)分ループ
-		use_speakers = random.sample(speaker_list, 6)
-		resemble_print(use_speakers)
-		secondhand_wav_file.append(use_speakers)
-		winner_neurons = []
+		therd_neuron = []
+		mapping_list = []
 
-		second_therd_synapse = np.zeros(par.kSecondLayerNuerons_)
+		for syllable_num in range(len(mapping_path[0])): #単音節の数(F1のファイル数)分ループ
+			use_speakers = random.sample(speaker_list, 6)
+			resemble_print(use_speakers)
+			secondhand_wav_file.append(use_speakers)
+			winner_neurons = []
 
-		for speaker in use_speakers:
-			resemble_print(str(speaker) + " : " + str(syllable_num) + " : " + str(mapping_path[speaker][syllable_num]))
-			count_neuron_fire = winner_take_all(synapse, mapping_path[speaker][syllable_num])
-			num_neuron_fire = sum(count_neuron_fire)
-			print(num_neuron_fire)
+			second_therd_synapse = np.zeros(par.kSecondLayerNuerons_)
 
-			second_therd_synapse += count_neuron_fire / num_neuron_fire
+			for speaker in use_speakers:
+				resemble_print(str(speaker) + " : " + str(syllable_num) + " : " + str(mapping_path[speaker][syllable_num]))
+				count_neuron_fire = winner_take_all(synapse, mapping_path[speaker][syllable_num])
+				num_neuron_fire = sum(count_neuron_fire)
+				print(num_neuron_fire)
 
-		second_therd_synapse = second_therd_synapse / len(use_speakers)
-		print(second_therd_synapse)
-		therd_neuron.append(second_therd_synapse)
-		mapping_list.append(extract_label(mapping_path[speaker][syllable_num]))
+				second_therd_synapse += count_neuron_fire / num_neuron_fire
 
-	second_therd_synapse_path = "2-3synapse/" + input_synaps
-	export_list2txt(therd_neuron, second_therd_synapse_path)
+			second_therd_synapse = second_therd_synapse / len(use_speakers)
+			print(second_therd_synapse)
+			therd_neuron.append(second_therd_synapse)
+			mapping_list.append(extract_label(mapping_path[speaker][syllable_num]))
 
+		second_therd_synapse_path = "2-3synapse/" + input_synaps
+		export_list2txt(therd_neuron, second_therd_synapse_path)
+
+	elif mode == "-c":
+		print("check")
+		second_therd_synapse = import_synapse("2-3synapse/" + str(input_synaps) + ".txt")
