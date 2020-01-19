8c9e89997e529c5ea84d721b3ccb457764f54613
Yuya <b1614196@planet.kanazawa-it.ac.jp>
2020-01-19 22:09:24 +0900

    ✨ sort on label

---
+++ 2layer_voice_reco/run-CPU/label_sort.py
@@ -0,0 +1,142 @@
+
+#ラベルソート(仮)
+def label_sort(target_list):
+    sorted_label = []
+    sorted_label.append(target_list[0]) #あ
+    sorted_label.append(target_list[40]) #い
+    sorted_label.append(target_list7[70]) #う
+    sorted_label.append(target_list[60]) #え
+    sorted_label.append(target_list[95]) #お
+
+    sorted_label.append(target_list[35]) #か
+    sorted_label.append(target_list[80]) #き
+    sorted_label.append(target_list[55]) #く
+    sorted_label.append(target_list[96]) #け
+    sorted_label.append(target_list[97]) #こ
+
+    sorted_label.append(target_list[12]) #さ
+    sorted_label.append(target_list[25]) #し
+    sorted_label.append(target_list[47]) #す
+    sorted_label.append(target_list[17]) #せ
+    sorted_label.append(target_list[86]) #そ
+
+    sorted_label.append(target_list[67]) #た
+    sorted_label.append(target_list[92]) #ち
+    sorted_label.append(target_list[62]) #つ
+    sorted_label.append(target_list[27]) #て
+    sorted_label.append(target_list[48]) #と
+
+    sorted_label.append(target_list[57]) #な
+    sorted_label.append(target_list[88]) #に
+    sorted_label.append(target_list[58]) #ぬ
+    sorted_label.append(target_list[31]) #ね
+    sorted_label.append(target_list[50]) #の
+
+    sorted_label.append(target_list[2]) #は
+    sorted_label.append(target_list[51]) #ひ
+    sorted_label.append(target_list[68]) #ふ
+    sorted_label.append(target_list[82]) #へ
+    sorted_label.append(target_list[34]) #ほ
+
+    sorted_label.append(target_list[64]) #ま
+    sorted_label.append(target_list[23]) #み
+    sorted_label.append(target_list[83]) #む
+    sorted_label.append(target_list[44]) #め
+    sorted_label.append(target_list[9]) #も
+
+    sorted_label.append(target_list[14]) #や
+    sorted_label.append(target_list[52]) #ゆ
+    sorted_label.append(target_list[77]) #よ
+
+    sorted_label.append(target_list[24]) #ら
+    sorted_label.append(target_list[78]) #り
+    sorted_label.append(target_list[53]) #る
+    sorted_label.append(target_list[54]) #れ
+    sorted_label.append(target_list[39]) #ろ
+
+    sorted_label.append(target_list[79]) #わ
+    sorted_label.append(target_list[100]) #ん
+
+    sorted_label.append(target_list[1]) #が
+    sorted_label.append(target_list[15]) #ぎ
+    sorted_label.append(target_list[11]) #ぐ
+    sorted_label.append(target_list[16]) #げ
+    sorted_label.append(target_list[65]) #ご
+
+    sorted_label.append(target_list[61]) #ざ
+    sorted_label.append(target_list[56]) #じ
+    sorted_label.append(target_list[21]) #ず
+    sorted_label.append(target_list[66]) #ぜ
+    sorted_label.append(target_list[87]) #ぞ
+
+    sorted_label.append(target_list[91]) #だ
+    sorted_label.append(target_list[74]) #で
+    sorted_label.append(target_list[98]) #ど
+
+    sorted_label.append(target_list[89]) #ば
+    sorted_label.append(target_list[32]) #び
+    sorted_label.append(target_list[43]) #ぶ
+    sorted_label.append(target_list[7]) #べ
+    sorted_label.append(target_list[8]) #ぼ
+
+    sorted_label.append(target_list[42]) #ぱ
+    sorted_label.append(target_list[13]) #ぴ
+    sorted_label.append(target_list[49]) #ぷ
+    sorted_label.append(target_list[93]) #ぺ
+    sorted_label.append(target_list[38]) #ぽ
+
+    sorted_label.append(target_list[10]) #きゃ
+    sorted_label.append(target_list[71]) #きゅ
+    sorted_label.append(target_list[41]) #きょ
+
+    sorted_label.append(target_list[45]) #ぎゃ
+    sorted_label.append(target_list[75]) #ぎゅ
+    sorted_label.append(target_list[72]) #ぎょ
+
+    sorted_label.append(target_list[90]) #しゃ
+    sorted_label.append(target_list[46]) #しゅ
+    sorted_label.append(target_list[85]) #しょ
+
+    sorted_label.append(target_list[73]) #じゃ
+    sorted_label.append(target_list[20]) #じゅ
+    sorted_label.append(target_list[26]) #じょ
+
+    sorted_label.append(target_list[81]) #ちゃ
+    sorted_label.append(target_list[30]) #ちゅ
+    sorted_label.append(target_list[5]) #ちょ
+
+    sorted_label.append(target_list[28]) #にゃ
+    sorted_label.append(target_list[99]) #にゅ
+    sorted_label.append(target_list[6]) #にょ
+
+    sorted_label.append(target_list[63]) #ひゃ
+    sorted_label.append(target_list[59]) #ひゅ
+    sorted_label.append(target_list[3]) #ひょ
+
+    sorted_label.append(target_list[76]) #ぴゃ
+    sorted_label.append(target_list[22]) #ぴゅ
+    sorted_label.append(target_list[18]) #ぴょ
+
+    sorted_label.append(target_list[36]) #びゃ
+    sorted_label.append(target_list[33]) #びゅ
+    sorted_label.append(target_list[37]) #びょ
+
+    sorted_label.append(target_list[4]) #みゃ
+    sorted_label.append(target_list[19]) #みゅ
+    sorted_label.append(target_list[94]) #みょ
+
+    sorted_label.append(target_list[69]) #りゃ
+    sorted_label.append(target_list[84]) #りゅ
+    sorted_label.append(target_list[29]) #りょ
+
+    sorted_label.append(target_list[101]) #シェ
+    sorted_label.append(target_list[102]) #ジェ
+    sorted_label.append(target_list[103]) #ティ
+    sorted_label.append(target_list[104]) #ファ
+    sorted_label.append(target_list[105]) #フォ
+    sorted_label.append(target_list[106]) #チェ
+    sorted_label.append(target_list[107]) #ディ
+    sorted_label.append(target_list[108]) #フィ
+    sorted_label.append(target_list[109]) #フェ
+
+    return sorted_label
